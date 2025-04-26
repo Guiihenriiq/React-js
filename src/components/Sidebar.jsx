d0dd343173a6b2e4a0b37870204034febfa4e13e
Guiihenriiq <ti3@ciclocairu.com.br>
2025-04-26 12:03:41 -0300

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,26 @@
 import styles from './Sidebar.module.css'
 
-export function Sidebar(){
+export function Sidebar() {
     return (
-     <aside className={styles.sidebar}></aside>   
+        <aside className={styles.sidebar}>
+            <img className={styles.cover}
+                src="https://images.unsplash.com/photo-1625838144804-300f3907c110?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src="https://github.com/Guiihenriiq.png" />
+
+                <strong>GuiiHenriiq</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+
     )
 }
