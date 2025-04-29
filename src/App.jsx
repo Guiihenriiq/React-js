7cb8cff2e7cb2cdc3511f8731074d1c4a22f8a21
Guiihenriiq <ti3@ciclocairu.com.br>
2025-04-29 11:58:51 -0300

    Propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,40 @@ import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
 
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/AlvesHamabily.png',
+      name: 'Hamabily Alves',
+      role: 'Full Stack Developer'
+    },
+    content: [
+      { type: 'paragraph', content: ' Fala galeraa 👋', },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2025-04-29 20:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/fragoso-dev.png',
+      name: 'Pablo Fragoso',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: ' Fala galeraa 👋', },
+
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2025-04-25 20:00:00'),
+  },
+]
+
 
 function App() {
   return (
@@ -14,14 +48,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            autor="Diego Fernandes"
-            content="Loren ipsum dolor sit amet consectetur adipisicing elit. Assumeda odio aliquam voluptate recusandae,non laborium dolore,excepturi iure repellat veniam modi, aliquid tempora facere libero cupididate unde exercitationem repellendus! Fugiat!"
-          />
-          <Post
-            autor="Gabriel Buzzi  "
-            content="Um novo post muito Legal"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,28 +2,22 @@ import styles from './Post.module.css'
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
 
-export function Post() {
+export function Post({ author, publishedAt }) {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/AlvesHamabily.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Alves Hamabily</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
                 <time title='28 de Abril de 2025' dateTime='2025-04-28 15:58:38'>Publicado há 1h</time>
             </header>
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 {' '}<a href="">jane.design/doctorcare</a></p>
-                <p> <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>{' '}
-                </p>
+
             </div>
             <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
@@ -36,9 +30,9 @@ export function Post() {
                 </footer>
             </form>
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
